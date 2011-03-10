1af62380973fddc49a7a59f997c5188005261ad0
Daniel Convissor <danielc@analysisandsolutions.com>
2011-03-10 14:22:57 -0500

    Add date format to cache key in list class.

--- include/CalendarSolution/List/List.php
+++ include/CalendarSolution/List/List.php
@@ -189,7 +189,8 @@ class CalendarSolution_List_List extends CalendarSolution_List {
 		if ($this->use_cache) {
 			$this->set_where_sql();
 
-			$cache_key = $this->cache_key . ':list:';
+			$cache_key = $this->cache_key . ':list:'
+				. $this->date_format;
 
 			$out = $this->cache->get($cache_key);
 			if ($out !== false) {
