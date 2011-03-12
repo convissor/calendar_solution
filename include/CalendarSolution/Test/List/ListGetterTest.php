d76f1153d8625d56a4d101b3e4251b19810f10f3
Daniel Convissor <danielc@analysisandsolutions.com>
2011-03-11 21:23:53 -0500

    Further date and limit nav tests.

--- include/CalendarSolution/Test/List/ListGetterTest.php
+++ include/CalendarSolution/Test/List/ListGetterTest.php
@@ -51,18 +51,20 @@ class CalendarSolution_Test_List_ListGetterTest extends PHPUnit_Framework_TestCa
 	}
 
 	protected function get_date_navigation_expected($prior_from, $prior_to,
-			$next_from, $next_to, $uri = '?view=List&amp;')
+			$next_from, $next_to, $uri = '?view=List&amp;',
+			$prior_text = '&lt; See Earlier Events',
+			$next_text = 'See Later Events &gt;')
 	{
 		$prior_link = '';
 		if ($prior_from) {
 			$prior_link = '<a href="' . $uri . 'from=' . $prior_from
-				. '&amp;to=' . $prior_to . '">&lt; See Earlier Events</a>';
+				. '&amp;to=' . $prior_to . '">' . $prior_text . '</a>';
 		}
 
 		$next_link = '';
 		if ($next_from) {
 			$next_link = '<a href="' . $uri . 'from=' . $next_from
-				. '&amp;to=' . $next_to . '">See Later Events &gt;</a>';
+				. '&amp;to=' . $next_to . '">' . $next_text . '</a>';
 		}
 
 		return '<div class="cs_date_navigation"><div class="cs_prior">'
@@ -70,17 +72,19 @@ class CalendarSolution_Test_List_ListGetterTest extends PHPUnit_Framework_TestCa
 			. '</div><div class="cs_next">' . $next_link . "</div></div>\n";
 	}
 
-	protected function get_limit_navigation_expected($prior, $next, $uri = '?') {
+	protected function get_limit_navigation_expected($prior, $next, $uri = '?',
+			$prior_text = '&lt; prior', $next_text = 'next &gt;')
+	{
 		$prior_link = '';
 		if (is_numeric($prior)) {
 			$prior_link = '<a href="' . $uri . 'limit_start=' . $prior . '">'
-				. '&lt; prior</a>';
+				. $prior_text . '</a>';
 		}
 
 		$next_link = '';
 		if ($next) {
 			$next_link = '<a href="' . $uri . 'limit_start=' . $next . '">'
-				. 'next &gt;</a>';
+				. $next_text . '</a>';
 		}
 
 		return '<div class="cs_limit_navigation"><div class="cs_prior">'
@@ -175,6 +179,15 @@ class CalendarSolution_Test_List_ListGetterTest extends PHPUnit_Framework_TestCa
 				'2010-11-01', '2011-01-31', '2011-05-01', '2011-07-31');
 		$this->assertEquals($expect, $actual);
 	}
+	public function test_get_date_navigation_parameters() {
+		$this->calendar->set_from('2011-02-01');
+		$this->calendar->set_to('2011-04-30');
+		$actual = $this->calendar->get_date_navigation('PRIOR', 'NEXT');
+		$expect = $this->get_date_navigation_expected(
+				'2010-11-01', '2011-01-31', '2011-05-01', '2011-07-31',
+				'?view=List&amp;', 'PRIOR', 'NEXT');
+		$this->assertEquals($expect, $actual);
+	}
 	public function test_get_date_navigation_history_0() {
 		$this->calendar->set_permit_history_months(0);
 
@@ -262,6 +275,13 @@ class CalendarSolution_Test_List_ListGetterTest extends PHPUnit_Framework_TestCa
 		$this->calendar->set_limit(10, 23);
 		$actual = $this->calendar->get_limit_navigation();
 		$expect = $this->get_limit_navigation_expected(13, false);
+		$this->assertEquals($expect, $actual);
+	}
+	public function test_get_limit_navigation_parameters() {
+		$this->calendar->set_limit(10, 3);
+		$actual = $this->calendar->get_limit_navigation('PRIOR', 'NEXT');
+		$expect = $this->get_limit_navigation_expected(0, 13, '?',
+				'PRIOR', 'NEXT');
 		$this->assertEquals($expect, $actual);
 	}
 	/**#@-*/
