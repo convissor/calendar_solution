7beee608682e7f490ca2ce5ee92e3bec13bedd82
Daniel Convissor <danielc@analysisandsolutions.com>
2011-03-16 13:54:48 -0400

    Oy vey, updated DateTime Solution was only in testing branch.

--- include/DateIntervalSolution.php
+++ include/DateIntervalSolution.php
@@ -4,44 +4,45 @@
  * Declares the DateIntervalSolution class based on the PHP version being used
  *
  * PHP Version:
- * + 5.3: DateIntervalSolution extends DateInterval
- * + 5.2: DateIntervalSolution extends DateTimeSolution_DateInterval
+ * + 5.4: DateIntervalSolution extends DateInterval
+ * + 5.3 & 5.2: DateIntervalSolution extends DateTimeSolution_DateInterval
  *
  * DateTime Solution is a trademark of The Analysis and Solutions Company.
  *
  * @package DateTimeSolution
  * @author Daniel Convissor <danielc@analysisandsolutions.com>
- * @copyright The Analysis and Solutions Company, 2009-2010
+ * @copyright The Analysis and Solutions Company, 2009-2011
  * @license http://www.analysisandsolutions.com/software/license.htm Simple Public License
  * @link http://www.analysisandsolutions.com/software/datetime_solution/
  */
 
-if (class_exists('DateInterval', false)) {
-	// PHP 5.3
-
+if (version_compare(phpversion(), '5.4', '<')) {
 	/**
-	 * PHP's DateInterval class exists; just stub it out
+	 * PHP 5.2 lacks DateInterval while in PHP 5.3 properties aren't reliably
+	 * writable; provide one for forward compatibility
+	 *
+	 * For example, PHP Bug 53634.
+	 *
+	 * @package DateTimeSolution
+	 * @author Daniel Convissor <danielc@analysisandsolutions.com>
+	 * @copyright The Analysis and Solutions Company, 2009-2011
+	 * @license http://www.analysisandsolutions.com/software/license.htm Simple Public License
+	 * @link http://www.analysisandsolutions.com/software/datetime_solution/
+	 */
+	class DateIntervalSolution extends DateTimeSolution_DateInterval {}
+} else {
+	/**
+	 * PHP 5.4 has everything; just stub out PHP's DateInterval
 	 * @ignore
 	 * @package DateTimeSolution
 	 */
 	class DateIntervalSolution extends DateInterval {
 		/**
 		 * Indicates which level of support the DateTime Solution is providing
-		 * @var string
+		 * @return string
 		 */
-		public $datetime_solution_level = 'native';
+		public function get_datetime_solution_level() {
+			return 'native';
+		}
 	}
-} else {
-	// PHP 5.2
-
-	/**
-	 * PHP 5.2 lacks DateInterval; provide one for forward compatibility
-	 *
-	 * @package DateTimeSolution
-	 * @author Daniel Convissor <danielc@analysisandsolutions.com>
-	 * @copyright The Analysis and Solutions Company, 2009-2010
-	 * @license http://www.analysisandsolutions.com/software/license.htm Simple Public License
-	 * @link http://www.analysisandsolutions.com/software/datetime_solution/
-	 */
-	class DateIntervalSolution extends DateTimeSolution_DateInterval {}
 }
--- include/DateTimeSolution/DateInterval.php
+++ include/DateTimeSolution/DateInterval.php
@@ -1,29 +1,23 @@
 <?php
 
 /**
- * DateTime Solution's DateInterval class for use if PHP < 5.3
+ * DateTime Solution's DateInterval class for use if PHP < 5.4
  *
  * @package DateTimeSolution
  * @author Daniel Convissor <danielc@analysisandsolutions.com>
- * @copyright The Analysis and Solutions Company, 2009-2010
+ * @copyright The Analysis and Solutions Company, 2009-2011
  * @license http://www.analysisandsolutions.com/software/license.htm Simple Public License
  */
 
 /**
- * Provides DateInterval functionality for versions of PHP before 5.3
+ * Provides DateInterval functionality for versions of PHP before 5.4
  *
  * @package DateTimeSolution
  * @author Daniel Convissor <danielc@analysisandsolutions.com>
- * @copyright The Analysis and Solutions Company, 2009-2010
+ * @copyright The Analysis and Solutions Company, 2009-2011
  * @license http://www.analysisandsolutions.com/software/license.htm Simple Public License
  */
 class DateTimeSolution_DateInterval {
-	/**
-	 * Indicates which level of support the DateTime Solution is providing
-	 * @var string
-	 */
-	public $datetime_solution_level = '52';
-
 	/**
 	 * The total number of days
 	 *
@@ -75,12 +69,20 @@ class DateTimeSolution_DateInterval {
 	 */
 	public function __construct($interval_spec) {
 		$this->interval_spec = $interval_spec;
-		if (!preg_match('/^P((\d+)Y)?((\d+)M)?((\d+)D)?(T((\d+)H)?((\d+)M)?((\d+)S)?)?$/', $interval_spec, $match)) {
+		if (!preg_match('/^P((\d+)Y)?((\d+)M)?((\d+)(D|W))?(T((\d+)H)?((\d+)M)?((\d+)S)?)?$/', $interval_spec, $match)) {
 			throw new Exception("Invalid interval_spec $interval_spec");
 		}
 		$this->y = empty($match[2]) ? 0 : (int) $match[2];
 		$this->m = empty($match[4]) ? 0 : (int) $match[4];
-		$this->d = empty($match[6]) ? 0 : (int) $match[6];
+		if (empty($match[6])) {
+			$this->d = 0;
+		} else {
+			if ($match[7] == 'W') {
+				$this->d = $match[6] * 7;
+			} else {
+				$this->d = (int) $match[6];
+			}
+		}
 		$this->h = empty($match[9]) ? 0 : (int) $match[9];
 		$this->i = empty($match[11]) ? 0 : (int) $match[11];
 		$this->s = empty($match[13]) ? 0 : (int) $match[13];
@@ -89,8 +91,9 @@ class DateTimeSolution_DateInterval {
 	/**
 	 * Formats the interval
 	 *
-	 * @param string $format  supports any combination of
-	 *                        "%y", "%m", "%d", "%a", "%h", "%i", "%s", "%R", "%r"
+	 * @param string $format  supports any combination of "%y", "%Y", "%m",
+	 *                        "%M", "%d", "%D", "%a", "%h", "%H", "%i", "%I",
+	 *                        "%s", "%S", "%r", "%R"
 	 * @return string
 	 */
 	public function format($format) {
@@ -102,8 +105,34 @@ class DateTimeSolution_DateInterval {
 			$this->r = '';
 		}
 
+		$days = $this->days === false ? '(unknown)' : $this->days;
+
 		$search = array('%y', '%m', '%d', '%a', '%h', '%i', '%s', '%R', '%r');
-		$replace = array($this->y, $this->m, $this->d, $this->days, $this->h, $this->i, $this->s, $this->R, $this->r);
-		return str_replace($search, $replace, $format);
+		$replace = array($this->y, $this->m, $this->d, $days, $this->h, $this->i, $this->s, $this->R, $this->r);
+		$format = str_replace($search, $replace, $format);
+
+		return preg_replace_callback('/%[YMDHIS]/',
+				array($this, 'replace_upper_case_formats'), $format);
+	}
+
+	/**
+	 * Indicates which level of support the DateTime Solution is providing
+	 *
+	 * Can't use a property because of PHP bug 52738
+	 *
+	 * @return string
+	 */
+	public function get_datetime_solution_level() {
+		return 'userland';
+	}
+
+	/**
+	 * Handles upper case formats for format()
+	 * @param array $matches  the data from preg_replace_callback()
+	 * @return string  the formatted number
+	 */
+	private function replace_upper_case_formats($matches) {
+		$property = substr(strtolower($matches[0]), -1);
+		return sprintf('%02d', $this->{$property});
 	}
 }
